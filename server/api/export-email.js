db6c9f91cecbb28a385f5964579b802a719b9160
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-11-18 12:04:03 +0100

    change db to supabase

--- server/api/export-email.js
+++ server/api/export-email.js
@@ -1,8 +1,16 @@
-import db from '../database.js';
+import { defineEventHandler } from 'h3';
+import { supabase } from '../utils/supabase';
 
 export default defineEventHandler(async (event) => {
   try {
-    const emails = db.prepare('SELECT * FROM emails').all();
+    const { data: emails, error } = await supabase
+      .from('emails')
+      .select('*');
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
     return emails;
   } catch (error) {
     return sendError(event, createError({ statusCode: 500, statusMessage: 'Erreur lors de l\'export des emails.' }));
